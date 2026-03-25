6ce44f7611fc7fc4ae511f96fc62b2ee11c77241
Oscar Mata Alegria <oscar_1322134219@uptecamac.edu.mx>
2026-03-24 20:56:27 -0600

    fix config vite on client

--- almacenaifa.client/vite.config.ts
+++ almacenaifa.client/vite.config.ts
@@ -53,9 +53,6 @@ export default defineConfig({
             }
         },
         port: parseInt(env.DEV_SERVER_PORT || '50262'),
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
+       
     }
 })
